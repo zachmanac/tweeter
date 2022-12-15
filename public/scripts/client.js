14e95882acb4ac731157b71924c4020ee8a60891
zachmanac <zachstephenson94@gmail.com>
2022-12-14 16:24:54 -0800

    add form submission with ajax using jquery method and post request

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,7 +5,7 @@
  */
 
 $(document).ready(function() {
-  const tweetList = [
+  const tweets = [
     {
       "user": {
         "name": "Newton",
@@ -29,44 +29,57 @@ $(document).ready(function() {
     }
   ];
 
-  const renderTweets = function(tweetList) {
+  const renderTweets = function(tweets) {
+    $('.tweet-section').empty();
     // loops through tweets
-    for(const tweet of tweetList) {
+    for(const tweet of tweets) {
       // calls createTweetElement for each tweet
       const $tweet = $createTweetElement(tweet);
+
       // takes return value and appends it to the tweets container
-      $('.tweet-section').append($tweet);
+      $('.tweet-section').prepend($tweet);
     }
   };
 
   const $createTweetElement = function (tweetData) {
-
     const tweet = `
-    <article class="tweet-container">
-    <header>
-      <div>
-      <h2><img src="${tweetData.user.avatars}" alt="${tweetData.user.name}'s Profile picture"></h2>
-      <h2>${tweetData.user.name}</h2>
-      </div>
-      <h4>${tweetData.user.handle}</h4>
-    </header>
-    <p>${tweetData.content.text}
-    </p>
-    <footer>
-      <p>${tweetData.created_at}</p>
-      <div>
-        <p>
-          <i class="fa-solid fa-flag"></i>
-          <i class="fa-solid fa-retweet"></i>
-          <i class="fa-solid fa-heart"></i>
-        </p>
-      </div>
-    </footer>
-    </article>
-    `
+      <article class="tweet-container">
+      <header>
+        <div>
+        <h2><img src="${tweetData.user.avatars}" alt="${tweetData.user.name}'s Profile picture"></h2>
+        <h2>${tweetData.user.name}</h2>
+        </div>
+        <h4>${tweetData.user.handle}</h4>
+      </header>
+      <p>${tweetData.content.text}
+      </p>
+      <footer>
+        <p>${tweetData.created_at}</p>
+        <div>
+          <p>
+            <i class="fa-solid fa-flag"></i>
+            <i class="fa-solid fa-retweet"></i>
+            <i class="fa-solid fa-heart"></i>
+          </p>
+        </div>
+      </footer>
+      </article>
+      `
     return tweet;
-    };
+  };
 
-  renderTweets(tweetList);
+  renderTweets(tweets);
+
+  $("form").on('submit', (event) => {
+    event.preventDefault();
+    const data = $("form").serialize();
+    console.log(data);
+
+    $.post("/tweets", data, (response) => {
+      console.log(response);
+    })
+
+  })
 
 });
+
